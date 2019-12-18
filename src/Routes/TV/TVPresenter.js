f649c55d276e6c5d33be808c68c6ac4a5b7c2e11
ddock4you <ddock4you@gmail.com>
2019-12-18 22:22:26 +0900

    - TVPresenter를 통해 TV 데이터 중 title 값을 받아옴

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,16 +1,36 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Loader from '../../Components/Loader';
+import Section from '../../Components/Section';
+
+const Container = styled.div`
+  padding: 0 10px;
+`;
 
 const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
-  null;
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title='TV TopRated'>{topRated.map(tv => <span>{tv.name}</span>)}</Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title='TV popular'>{popular.map(tv => <span>{tv.name}</span>)}</Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title='TV airingToday'>{airingToday.map(tv => <span>{tv.name}</span>)}</Section>
+      )}
+    </Container>
+  );
 
 TVPresenter.propTypes = {
   topRated: PropTypes.array,
   popular: PropTypes.array,
   airingToday: PropTypes.array,
   loading: PropTypes.bool.isRequired,
-  error: PropTypes.string
+  error: PropTypes.string,
 };
 
 export default TVPresenter;
