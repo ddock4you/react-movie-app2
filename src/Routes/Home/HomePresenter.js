f5655d430b1b2acbb278ec08afbb246d3f7e8bcf
ddock4you <ddock4you@gmail.com>
2019-12-19 16:53:44 +0900

    - Poster 컴포넌트 추가, HomePresenter에 Poster 컴포넌트를 보이게 추가 중

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,44 +1,69 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
-import Section from "../../Components/Section";
-import Loader from "../../Components/Loader";
-import Message from "../../Components/Message";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Section from '../../Components/Section';
+import Loader from '../../Components/Loader';
+import Message from '../../Components/Message';
+import Poster from '../../Components/Poster';
 
 const Container = styled.div`
-    padding: 0px 20px;
+  padding: 0px 20px;
 `;
 
 const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
-    loading ? <Loader/> : (
-        <Container>
-            {nowPlaying && nowPlaying.length > 0 && (
-                <Section title="Now Playing">
-                    {nowPlaying.map(movie => <span>{movie.title}</span>)}
-                </Section>
-            )}
-            {popular && popular.length > 0 && (
-                <Section title="Popular Movies">
-                    {popular.map(movie => <span>{movie.title}</span>)}
-                </Section>
-            )}
-            {upcoming && upcoming.length > 0 && (
-                <Section title="Upcoming Movies">
-                    {upcoming.map(movie => <span>{movie.title}</span>)}
-                </Section>
-            )}
-            {error && <Message color="#e74c3c" text={error} />}
-        </Container>
-    );
-
-
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title='Now Playing'>
+          {nowPlaying.map(movie => (
+            <Poster
+              id={movie.id}
+              title={movie.title}
+              imageUrl={movie.imageUrl}
+              rating={movie.rating}
+              year={movie.year}
+            ></Poster>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title='Popular Movies'>
+          {popular.map(movie => (
+            <Poster
+              id={movie.id}
+              title={movie.title}
+              imageUrl={movie.imageUrl}
+              rating={movie.rating}
+              year={movie.year}
+            ></Poster>
+          ))}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title='Upcoming Movies'>
+          {upcoming.map(movie => (
+            <Poster
+              id={movie.id}
+              title={movie.title}
+              imageUrl={movie.imageUrl}
+              rating={movie.rating}
+              year={movie.year}
+            ></Poster>
+          ))}
+        </Section>
+      )}
+      {error && <Message color='#e74c3c' text={error} />}
+    </Container>
+  );
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
   popular: PropTypes.array,
   upcoming: PropTypes.array,
   loading: PropTypes.bool.isRequired,
-  error: PropTypes.string
+  error: PropTypes.string,
 };
 
 export default HomePresenter;
